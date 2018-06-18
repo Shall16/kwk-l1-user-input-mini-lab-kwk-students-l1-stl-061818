649678203a5c92dab9e3c2922f018944907f021f
Shall16 <shall130@rsdmo.org>
2018-06-18 20:13:04 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,6 +1,4 @@
-# Code your prompts here!
 
-# First, puts out a string asking where the tourist would like to stay.
 puts "Let's plan your trip! Where would you like to stay?"
 
 # Then, set a variable called stay to get the user's input. Make sure to capitilize that input.
